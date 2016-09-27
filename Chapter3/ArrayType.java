c4d130cb896ca96379db5b610549cd53140629ad
Boomatang <Jimfity@gmail.com>
2016-09-27 18:17:17 +0100

    Finished Chapter3
    Ready to take sample test

---
+++ Chapter3/ArrayType.java
@@ -0,0 +1,10 @@
+package Chapter3;
+
+public class ArrayType {
+	public static void main(String[] args) {
+		String [] bugs = {"cricket", "beetle", "ladybug"};
+		String [] alias = bugs;
+		System.out.println(bugs.equals(alias));
+		System.out.println(bugs.toString());
+	}
+}
